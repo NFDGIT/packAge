20d5c1a1b5bc2587497fcc0b0256d699d742b41b
Admin <admin@AdmindeMacBook-Pro-9.local>
2019-02-12 15:12:19 +0800

    first commit

---
+++ package/Tools/Constant.swift
@@ -0,0 +1,16 @@
+//
+//  Constant.swift
+//  package
+//
+//  Created by Admin on 2019/2/12.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+class Constant: NSObject {
+
+}
