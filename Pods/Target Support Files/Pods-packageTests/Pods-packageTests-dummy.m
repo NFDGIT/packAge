15c08fdbe025d61c3f67644cb897eb82e617269b
Admin <admin@bogon>
2019-02-12 17:59:48 +0800

    add pod

---
+++ Pods/Target Support Files/Pods-packageTests/Pods-packageTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_packageTests : NSObject
+@end
+@implementation PodsDummy_Pods_packageTests
+@end
--- package/Tools/Constant.swift
+++ package/Tools/Constant.swift
@@ -8,9 +8,6 @@
 
 import UIKit
 
-
-
-
 class Constant: NSObject {
 
 }
