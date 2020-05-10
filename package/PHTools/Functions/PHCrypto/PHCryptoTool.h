aeeb4c0919596f56c8dbfbdec091ce7e1c66a96c
彭辉 <942261721@qq.com>
2020-05-10 17:49:16 +0800

    添加 加密文件

---
+++ package/PHTools/Functions/PHCrypto/PHCryptoTool.h
@@ -0,0 +1,17 @@
+//
+//  PHCryptoTool.h
+//  package
+//
+//  Created by penghui on 2020/5/10.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+/**加密 类*/
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PHCryptoTool : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ package/PHTools/Functions/PHCrypto/PHCryptoTool.m
@@ -0,0 +1,13 @@
+//
+//  PHCryptoTool.m
+//  package
+//
+//  Created by penghui on 2020/5/10.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+#import "PHCryptoTool.h"
+
+@implementation PHCryptoTool
+
+@end
